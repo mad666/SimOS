7ebd2363bb0e2a4638a50ecd0ecea064761c396c
mad666 <maxrichter@gmx.de>
2011-12-17 11:23:31 +0100

    Vererbung eingerichtet

--- src/MemoryManagement/InvPTEntry.java
+++ src/MemoryManagement/InvPTEntry.java
@@ -1,5 +1,20 @@
 package MemoryManagement;
 
-public class InvPTEntry {
+public class InvPTEntry extends PTEntry {
 
+	private int pid;
+	private int pageindex;
+
+	public InvPTEntry(int pid, int pageindex) {
+		this.pid = pid;
+		this.pageindex = pageindex;
+	}
+
+	public int getPid() {
+		return pid;
+	}
+
+	public int getPageIndex() {
+		return pageindex;
+	}
 }
